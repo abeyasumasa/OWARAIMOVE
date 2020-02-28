5292e13ff32fcff6fa22687aedbaa2f8a3f963c9
abeyasumasa <abeyasumasa@gmail.com>
2020-02-29 02:18:45 +0900

    Add:ユーザー,芸人,ライブのシードデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ユーザー
+# 芸人ではないユーザー
+15.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               entertainer: false,
+               icon: open("#{Rails.root}/db/fixtures/image01.jpg")
+               )
+end
+
+# 芸人であるユーザー
+15.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               entertainer: true,
+               icon: open("#{Rails.root}/db/fixtures/image01.jpg")
+               )
+end
+
+# 芸人
+
+30.times do |n|
+  name = Faker::Games::Zelda.character
+  email = Faker::Internet.email
+  user_id = rand(15..30)
+  Comedian.create!(combination_name: name,
+               email: email,
+               genre: "漫才",
+               combination_icon: open("#{Rails.root}/db/fixtures/image02.jpg"),
+               comment: "test",
+               user_id: user_id
+               )
+end
+
+# ライブ
+30.times do |n|
+  title = Faker::Book.title
+  Live.create!(title: title,
+               content: "test",
+               place: "渋谷",
+               date: DateTime.parse("2020/02/29"),
+               start_time: Time.strptime("2020/02/29 10:00","%Y/%m/%d %H:%M"),
+               ending_time: Time.strptime("2020/02/29 12:00","%Y/%m/%d %H:%M"),
+               price: 2000
+               )
+end
