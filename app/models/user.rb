7ce86432657af44ff515f2ef657d1c37f6f013f5
abeyasumasa <abeyasumasa@gmail.com>
2020-03-17 23:40:49 +0900

    アクセス管理の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,33 +18,34 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable,:omniauthable, omniauth_providers: %i(google)
+         :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: %i(google)
   # イメージアップローダー機能
-  mount_uploader :icon ,UserImageUploader
+  mount_uploader :icon, UserImageUploader
 
   # バリデーション
-  validates :name,  presence: true, length: { maximum: 30 }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true, length: {maximum: 30}
+  validates :password, presence: true, length: {minimum: 6}, on: :create
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 
   # ランダムなuidを作成
   def self.create_unique_string
     SecureRandom.uuid
   end
+
   # 外部から取得したユーザー情報を元に、このアプリで使用するユーザーを作成するメソッド
   def self.find_for_google(auth)
     user = User.find_by(email: auth.info.email)
     unless user
       user = User.new(email: auth.info.email,
                       provider: auth.provider,
-                      uid:      auth.uid,
-                      name:     auth.info.name,
+                      uid: auth.uid,
+                      name: auth.info.name,
                       password: Devise.friendly_token[0, 20],
-                                   )
+      )
     end
     user.save
     user
   end
-                             
+
 end
