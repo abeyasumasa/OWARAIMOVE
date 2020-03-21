8f34ca34f01ddcf1740ac6314b0f5715e19ac3cf
abeyasumasa <abeyasumasa@gmail.com>
2020-03-21 10:38:01 +0900

    Fixed:お気に入りが重複しないようモデルテストを修正

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe Favorite, type: :model do
   let!(:first_user) { create(:first_user) }
   let!(:second_user) { create(:second_user) }
   let!(:favorite) { create(:favorite, user_id: first_user.id, comedian_id: second_user.comedian.id) }
+  let(:favorite2) { FactoryBot.build(:favorite, user_id: first_user.id, comedian_id: second_user.comedian.id) }
   it '重複してお気に入りが出来る場合、無効である' do
-    favorite.user.id = '3'
-    favorite.user.id = '3'
-    expect(favorite).to_not be_valid
+    expect(favorite2).to_not be_valid
   end
 end
 
