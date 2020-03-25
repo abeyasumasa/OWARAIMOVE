d7dd3fdba531669ff75eabd6f8a726914e8c86b5
abeyasumasa <abeyasumasa@gmail.com>
2020-03-25 13:57:03 +0900

    画面レイアウト修正とシステムスペックテストの 修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module OWARAIMOVE
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.generators do |g|
       g.test_framework :rspec,
@@ -20,7 +22,5 @@ module OWARAIMOVE
                        controller_specs: false,
                        request_specs: false
       end
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
   end
 end
