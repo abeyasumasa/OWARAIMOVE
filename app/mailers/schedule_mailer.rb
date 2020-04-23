95545da9fd0140963c1cfe5102fc14c63f4a58ba
abeyasumasa <abeyasumasa@gmail.com>
2020-04-24 00:34:31 +0900

    Add:予定通知機能を 実装

---
+++ app/mailers/schedule_mailer.rb
@@ -0,0 +1,10 @@
+class ScheduleMailer < ApplicationMailer
+  def schedule_mail
+    @live = Live.where(date: Date.today.since(1.days))
+    @live.each do |live|
+      live.participant_management_users.each do |user|
+        mail to:user.email, subject:"参加予定ライブ通知のメール"
+      end
+    end
+  end
+end
---
+++ spec/mailers/previews/schedule_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/schedule_mailer
+class ScheduleMailerPreview < ActionMailer::Preview
+
+end
