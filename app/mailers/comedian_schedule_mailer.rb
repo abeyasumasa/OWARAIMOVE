44d2c983111b857b1b4c5b6e0ac53fefbcd48764
abeyasumasa <abeyasumasa@gmail.com>
2020-05-03 10:39:13 +0900

    Fixed:芸人に明日開催予定のライブを通知する

--- app/mailers/comedian_schedule_mailer.rb
+++ app/mailers/comedian_schedule_mailer.rb
@@ -1,10 +1,23 @@
 class ComedianScheduleMailer < ApplicationMailer
+  before_action :set_lives
+
   def comedian_schedule_mail
-    @live = Live.where(date: Date.today.since(1.days))
-    @live.each do |live|
+    mail to:@comedian.email, subject:"出演予定ライブ通知のメール"
+  end
+
+  private
+
+  def set_lives
+    @lives = Live.where(date: Date.today.since(1.days))
+    set_comedian(@lives)
+  end
+
+  def set_comedian(lives)
+    lives.each do |live|
       live.performer_management_comedians.each do |comedian|
-        mail to:comedian.email, subject:"出演予定ライブ通知のメール"
+        @comedian = comedian
       end
     end
   end
+
 end
