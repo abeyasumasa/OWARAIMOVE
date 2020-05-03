c345c583707f3ff910af60d12fb2e020eeb2bcb4
abeyasumasa <abeyasumasa@gmail.com>
2020-05-03 21:53:45 +0900

    gem wheneverの設定を変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
+set :output, "log/crontab.log"
+set :environment, :production
+
 every 1.days, at: "8:00 am" do
   runner "UserScheduleMailer.user_schedule_mail.deliver_now"
   runner "ComedianScheduleMailer.comedian_schedule_mail.deliver_now"
