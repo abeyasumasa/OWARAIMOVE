a5f834bd9731c3533a444af2949b7efa29abfc75
abeyasumasa <abeyasumasa@gmail.com>
2020-06-06 20:22:51 +0900

    Fixed:レスポンシブ対応できるように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @schedule = @user.participant_management_lives.page(params[:page]).per(3)
+    @schedule = @user.participant_management_lives.page(params[:page]).per(5)
   end
 
   def edit
