af4a72f4f400dcd7ae15bf288cd05677889926af
abeyasumasa <abeyasumasa@gmail.com>
2020-06-19 06:47:07 +0900

    Fixed: 本番環境のデフォルトURL変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "owaraimove.net" }
+  config.action_mailer.default_url_options = { host: "www.owaraimove.net" }
   config.action_mailer.delivery_method = :ses
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
