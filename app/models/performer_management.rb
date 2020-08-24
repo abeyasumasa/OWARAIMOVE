c3ae246c0fbefe2d4f4e7534ecd87cb6960469f5
abeyasumasa <abeyasumasa@gmail.com>
2020-08-25 01:00:22 +0900

    ライブに登録されている芸人を１人以下にならないように制限

--- app/models/performer_management.rb
+++ app/models/performer_management.rb
@@ -1,4 +1,15 @@
 class PerformerManagement < ApplicationRecord
   belongs_to :live
   belongs_to :comedian
+
+  before_destroy :do_not_destroy_perfomer_management_comedian
+
+  private
+
+  # バリデーション(ライブに出演する芸人を１人以下にしたくない)
+  def do_not_destroy_perfomer_management_comedian
+    if PerformerManagement.where(live_id:live_id).count == 1
+      throw :abort
+    end
+  end
 end
